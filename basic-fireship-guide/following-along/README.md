9ea4feb654f89a26c8a6e1c76cc560605b778108
Tobias Livadariu <tobias.livadariu@gmail.com>
2025-12-25 22:05:12 -0500

    Finished

---
+++ basic-fireship-guide/following-along/README.md
@@ -0,0 +1,3 @@
+# NOTE: THIS IS NOT MY CODE!!!
+
+i'm just following along a guide to try and learn the fundamentals of three.js!
--- basic-fireship-guide/following-along/src/main.js
+++ basic-fireship-guide/following-along/src/main.js
@@ -77,6 +77,26 @@ const moon = new THREE.Mesh(
 
 scene.add(moon)
 
+moon.position.z = 30;
+moon.position.setX(-10);
+
+function moveCamera() {
+  const t = document.body.getBoundingClientRect().top;
+  moon.rotation.x += 0.05;
+  moon.rotation.y += 0.075;
+  moon.rotation.z += 0.05;
+
+  jeff.position.y += 0.01;
+  jeff.position.z += 0.01;
+
+  camera.position.z = t * -0.01;
+  camera.position.x = t * -0.0002;
+  camera.position.y = t * -0.01;
+
+}
+
+document.body.onscroll = moveCamera
+
 function animate() {
   requestAnimationFrame(animate);
 
