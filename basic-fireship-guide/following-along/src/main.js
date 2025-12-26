aa99ecaedf0e83504a741cab22e4588a1b650293
Tobias Livadariu <tobias.livadariu@gmail.com>
2025-12-25 21:52:10 -0500

    Working with texture maps

--- basic-fireship-guide/following-along/src/main.js
+++ basic-fireship-guide/following-along/src/main.js
@@ -50,6 +50,33 @@ function addStar() {
 
 Array(200).fill().forEach(addStar)
 
+const spaceTexture = new THREE.TextureLoader().load('space.jpg');
+scene.background = spaceTexture
+
+const jeffTexture = new THREE.TextureLoader().load('jeff.png');
+
+const jeff = new THREE.Mesh(
+  new THREE.BoxGeometry(3, 3, 3),
+  new THREE.MeshBasicMaterial({
+    map: jeffTexture
+  })
+)
+
+scene.add(jeff);
+
+const moonTexture = new THREE.TextureLoader().load('moon.jpg');
+const normalTexture = new THREE.TextureLoader().load('normal.jpg')
+
+const moon = new THREE.Mesh(
+  new THREE.SphereGeometry(3, 32, 32),
+  new THREE.MeshStandardMaterial({
+    map: moonTexture,
+    normalMap: normalTexture
+  })
+)
+
+scene.add(moon)
+
 function animate() {
   requestAnimationFrame(animate);
 
